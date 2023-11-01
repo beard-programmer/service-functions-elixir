3bc5061a8331598b900ea4e4349832447601d42e
victorshinkevich <supostatekx@gmail.com>
2023-11-01 19:03:41 +0300

    specify where t store dializer plt

--- mix.exs
+++ mix.exs
@@ -7,7 +7,10 @@ defmodule ServiceFunctionsElixir.MixProject do
       version: "0.1.0",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      dialyzer: [
+        plt_file: {:no_warn, "priv/plts/service-functions-elixir.plt"}
+      ]
     ]
   end
 
